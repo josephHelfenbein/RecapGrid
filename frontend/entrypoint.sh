e4d3f5e412a2b8c4396eb28d8c2e3dd6358f1637
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-02-24 21:19:34 -0500

    fix for nuxt SSR

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-set -e
 
-envsubst '${API_URL}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
-mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
+envsubst '$API_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-exec nginx -g 'daemon off;'
+exec "$@"
