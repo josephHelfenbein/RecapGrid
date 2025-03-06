ac3069110699640fbb4246c32a08f2e84b625e6f
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-03-06 01:39:22 -0500

    add server responses

--- backend/src/main/java/com/recapgrid/App.java
+++ backend/src/main/java/com/recapgrid/App.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.recapgrid.model.ClerkUser;
 import com.recapgrid.model.UserEntity;
@@ -31,12 +36,13 @@ public class App {
     }
 
     @PostMapping("/clerk-user")
-    public String createClerkUser(@RequestBody ClerkUser clerkUser) {
+    public ResponseEntity<String> createClerkUser(@RequestBody ClerkUser clerkUser) {
         if(clerkUser == null || clerkUser.getData() == null) {
-            return "ClerkUser data is missing";
+            return ResponseEntity.badRequest().body("Error: ClerkUser data is missing!");
         }
         UserEntity entity = new UserEntity();
         entity.setId(clerkUser.getData().getId());
+        entity.setCreatedAt(OffsetDateTime.now());
         entity.setFullName(clerkUser.getData().getFirstName() + " " + clerkUser.getData().getLastName());
         entity.setProfilePicture(clerkUser.getData().getImageUrl());
         if(clerkUser.getData().getEmailAddresses()!=null && !clerkUser.getData().getEmailAddresses().isEmpty()) {
@@ -45,8 +51,12 @@ public class App {
             entity.setEmail(null);
         }
 
-        repository.save(entity);
-        return "User saved with name: " + entity.getFullName();
+        try {
+            repository.save(entity);
+            return ResponseEntity.ok("User saved with name: " + entity.getFullName());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving user: " + e.getMessage());
+        }
     }
 
 }
