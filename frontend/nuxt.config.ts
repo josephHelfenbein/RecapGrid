1b4b17bd9e5ade91c5cd176c1527d8837718cad0
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-03-11 15:54:41 -0400

    fix again

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_URL?.endsWith('/api/') ? process.env.NUXT_PUBLIC_API_URL : `${process.env.NUXT_PUBLIC_API_URL}/api`,
+      apiBase: process.env.NUXT_PUBLIC_API_URL,
     },
   },
   compatibilityDate: '2024-11-01',
