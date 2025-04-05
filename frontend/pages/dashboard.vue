f053dc276ef1bc166c7bf88c9cf81a31968fe1dc
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-04-05 01:47:58 -0400

    fix again

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -176,7 +176,7 @@
   const handleProcessing = async (data) =>{
     console.log(data);
     console.log(fileToProcess.value);
-    const response = await fetch("/api/videos/processVideo", {
+    const response = await fetch("/api/processVideo", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
