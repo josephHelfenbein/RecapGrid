a5bdccff73fa7af03875733f9c02934686d94d66
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-05-06 16:56:47 -0400

    possible fix for find by id

--- backend/src/main/java/com/recapgrid/repository/StatusRepository.java
+++ backend/src/main/java/com/recapgrid/repository/StatusRepository.java
@@ -4,5 +4,5 @@ import com.recapgrid.model.StatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatusRepository extends JpaRepository<StatusEntity, String> {
-    StatusEntity findByUserId(String userId);
+    StatusEntity findByUserId(String id);
 }
