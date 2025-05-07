417d8f0912e86cafe7fb125435b7c854ab05b4ac
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-05-06 23:32:02 -0400

    use different supabase plugin setup

--- frontend/plugins/supabase.client.ts
+++ frontend/plugins/supabase.client.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import { defineNuxtPlugin, useRuntimeConfig } from '#app'
+
 export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig();
     const supabaseUrl = config.public.supabaseUrl!.toString();
     const supabaseKey = config.public.supabaseKey!.toString();
   
-    const $supabase = createClient(supabaseUrl, supabaseKey);
-    nuxtApp.provide('$supabase', $supabase)
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    nuxtApp.provide('supabase', supabase)
   });
